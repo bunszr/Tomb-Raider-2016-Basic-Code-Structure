cbce148c20921aec478013264c4610b066d1757f
BUNYAMINSEZER\Sezer <bunyamin.ssezer@gmail.com>
2024-05-19 23:09:54 +0300

    Fixed "To prevent OnPointerExit is not called If pressed Esc key to close panel and mouse is over UI element"

--- Assets/_Game/Scripts/Camp Site/Commands/Controller/CampsiteCommandBase.cs
+++ Assets/_Game/Scripts/Camp Site/Commands/Controller/CampsiteCommandBase.cs
@@ -7,6 +7,9 @@ namespace CampSite
         protected CSBBase csbBase;
         protected ButtonEvents buttonEvents => csbBase.buttonEvents;
 
+        bool hasEnter;
+        bool hasExit;
+
         public CampsiteButtonCommandBase(CSBBase csbBase) => this.csbBase = csbBase;
 
         public virtual void OnActivate()
@@ -19,9 +22,11 @@ namespace CampSite
         {
             buttonEvents.onPointerEnterEvent -= OnPointerEnter;
             buttonEvents.onPointerExitEvent -= OnPointerExit;
+
+            if (hasEnter && !hasExit) OnPointerExit(new PointerEventData(EventSystem.current)); // To prevent OnPointerExit is not called If pressed Esc key to close panel and mouse is over UI element
         }
 
-        protected virtual void OnPointerEnter(PointerEventData eventData) { }
-        protected virtual void OnPointerExit(PointerEventData eventData) { }
+        protected virtual void OnPointerEnter(PointerEventData eventData) { hasEnter = true; hasExit = false; }
+        protected virtual void OnPointerExit(PointerEventData eventData) { hasEnter = false; hasExit = true; }
     }
 }
