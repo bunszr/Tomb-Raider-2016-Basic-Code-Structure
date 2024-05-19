ef156b0e6021344ca2fc0697da61508ef594ffc6
BUNYAMINSEZER\Sezer <bunyamin.ssezer@gmail.com>
2024-05-19 18:27:53 +0300

    Fixed first level button animation

--- Assets/_Game/Scripts/Camp Site/Commands/Views/FirstLevelButtonAnimationCommandView.cs
+++ Assets/_Game/Scripts/Camp Site/Commands/Views/FirstLevelButtonAnimationCommandView.cs
@@ -1,5 +1,6 @@
 using Cinemachine;
 using DG.Tweening;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace CampSite
@@ -13,15 +14,22 @@ namespace CampSite
         public FirstLevelButtonAnimationCommandView(CSBBase csbBase, CinemachineVirtualCamera cam) : base(csbBase)
         {
             this.cam = cam;
-            tween = csbBase.transform.DOLocalMoveZ(Data.zOffset, Data.duration).SetAutoKill(false).SetEase(Data.ease).Pause();
         }
 
         GameDataScriptable.CampSiteScriptableData.FirstLevelAnimScriptableData Data => GameDataScriptable.Ins.campSiteScriptableData.firstLevelAnimScriptableData;
+
+        public override void OnActivate()
+        {
+            base.OnActivate();
+            // csbBase.transform.SetLocalPosZ(0);
+            tween = csbBase.transform.DOLocalMoveZ(Data.zOffset, Data.duration).From(0).SetAutoKill(false).SetEase(Data.ease).Pause();
+        }
 
         protected override void OnPointerEnter(PointerEventData eventData)
         {
             // cam.LookAt = transform;
             tween.PlayForward();
+            Debug.Log("enter");
         }
 
         protected override void OnPointerExit(PointerEventData eventData)
